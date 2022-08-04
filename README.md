4339ff326ba3808e4c0d1df4d15b864af2713f45
Andresuito <barrancorios@gmail.com>
2022-08-04 12:25:05 +0200

    Weather v1.0

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# weather-app
+# Weather-app
+Application that shows you the weather for any location
+
+#### *Information*
+If you want to use it, you must register with Weatherapi and set up using their API Key. You will have to modify line 31 of the app.js file.
+
+#### *Resources*
+- Desing by mdbootstrap | **[Link](https://mdbootstrap.com/docs/standard/extended/weather/#!)**
+- Boostrap | **[Link](https://getbootstrap.com/)**
+- Font Awesome | **[Link](https://cdnjs.com/libraries/font-awesome)**
+- Weather icons | **[Link](https://www.weatherapi.com/docs/#weather-icons)**
+- API - Weatherapi | **[Link](https://www.weatherapi.com/)**
+
+#### *Features*
+- Add Sweetalert2
+- New Desing
+- More information
---
+++ assets/js/app.js
@@ -0,0 +1,57 @@
+
+const app = document.querySelector('.weather-app');
+const temp = document.querySelector('.temp');
+const dateOutput = document.querySelector('.date');
+const timeOutput = document.querySelector('.time');
+const conditionOutput = document.querySelector('.condition');
+const nameOutput = document.querySelector('.name');
+const icon = document.querySelector('.icon');
+const cloudOutput = document.querySelector('.cloud');
+const humidityOutput = document.querySelector('.humidity');
+const windOutput = document.querySelector('.wind');
+const TimeZoneOutput = document.querySelector('.tz');
+const form = document.getElementById('locationInput');
+const search = document.querySelector('.search');
+const btn = document.querySelectorAll('.submit');
+
+let cityInput = "Huelva";
+
+form.addEventListener('submit', (e) => {
+    if (search.value.length == 0) {
+        alert('Please type in a city name');
+    } else {
+        cityInput = search.value;
+        fetchWeatherData();
+        search.value = "";
+    }
+    e.preventDefault();
+});
+
+function fetchWeatherData() {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=YOUR API KEY&q=${cityInput}
+    `)
+
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+
+            temp.innerHTML = Math.floor(data.current.temp_c) + "ºC";
+            conditionOutput.innerHTML = data.current.condition.text;
+
+            const date = data.location.localtime;
+            const time = date.substr(11);
+
+            timeOutput.innerHTML = time;
+            nameOutput.innerHTML = data.location.name;
+
+            const iconId = data.current.condition.icon.substr("//cdn.weatherapi.com/weather/64x64".length);
+            icon.src = "./assets/img/weather" + iconId;
+
+            cloudOutput.innerHTML = data.current.cloud + "%";
+            humidityOutput.innerHTML = data.current.humidity + "%";
+            windOutput.innerHTML = data.current.wind_kph + " km/h";
+            TimeZoneOutput.innerHTML = data.location.tz_id;
+        });
+}
+
+fetchWeatherData();
